48fd90ca84ef6e16b2f787b71f67bb404ca2d7d3
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2013-02-20 20:42:56 -0500

    This revision adds the following features to this plugin: [1] the getID3 library for parsing MP3 files was added, [2] the Essentials and Interception_Manager classes were added to the include package, [3] the Audio_Caching_Manager class was fully completed, [4] the Hook_Manager class has now been placed under the FFI\AAM namespace, and [5] the Installer now have Javadoc style comments.

--- includes/Audio_Caching_Manager.php
+++ includes/Audio_Caching_Manager.php
@@ -1,10 +1,70 @@
 <?php
+/**
+ * Audio Caching Manager class
+ *
+ * This class will scan the archive directory and update the cache
+ * accordingly.
+ * 
+ * If a new file has been added to the archive directory, then the getID3
+ * library is used to parse the MP3 metadata and extract relevant information
+ * into cache.
+ * 
+ * If a file has been deleted from the archive directory, then the file
+ * information is also removed from cache.
+ *
+ * @author    Oliver Spryn
+ * @copyright Copyright (c) 2013 and Onwards, ForwardFour Innovations
+ * @license   MIT
+ * @namespace FFI\AAM
+ * @package   includes
+ * @since     v1.0 Dev
+*/
+
 namespace FFI\AAM;
 
+require_once(PATH . "/includes/getID3/getid3.php");
+
 class Audio_Caching_Manager {
-	private $cache;
-	private $files;
+/**
+ * Hold the list of items which are present in cache but are not on the file
+ * system
+ *
+ * @access private
+ * @type   array<string>
+*/
+	
+	private $cache = array();
+	
+/**
+ * Hold the list of items which are on the file system but are not present in 
+ * cache
+ *
+ * @access private
+ * @type   array<string>
+*/
+	
+	private $files = array();
+	
+/**
+ * Hold the address to the archive directory
+ *
+ * @access private
+ * @type   string
+*/
+	
 	private $rootDirectory;
+	
+/**
+ * CONSTRUCTOR
+ *
+ * This constructor bootstraps the functionality of the class. It will generate
+ * the address to the archive directory, cache new files from the file system, 
+ * and purge old files from the cache which no longer exist on the file system.
+ * 
+ * @access public
+ * @return void
+ * @since  v1.0 Dev
+*/
 	
 	public function __construct() {
 	//Get the location of the upload directory
@@ -12,19 +72,133 @@ class Audio_Caching_Manager {
 		$this->rootDirectory = $directory['basedir'] . "/audio-archive/";
 		
 		$this->diff();
+		$this->cacheNew();
+		$this->purgeOld();
 	}
 	
-	public function diff() {
+/**
+ * This method will iterate through the $this->files array and cache each
+ * of the listed MP3s into the database using the getID3 library. The following
+ * items from the metadata are cached into the ffi_aam_audiocache relation:
+ *  - title
+ *  - artist
+ *  - year
+ *  - audio play time
+ *  - file size
+ *  - file name
+ * 
+ * @access private
+ * @return void
+ * @since  v1.0 Dev
+*/
+	
+	private function cacheNew() {
+		global $wpdb;
+		
+		if (count($this->files)) {
+			$getID3 = new \getID3();
+			$fileData = array();
+			$size = 0;
+		
+			foreach($this->files as $file) {
+			//Fetch the metadata
+				$fileData = $getID3->analyze($this->rootDirectory . $file);
+				$title = "";
+				$artist = "";
+				$year = "";
+			
+			//Some of the metadata may not exist, so check it before usings its values
+				if (isset($fileData['id3v2']['comments']['title'])) {
+					$title = implode(", ", $fileData['id3v2']['comments']['title']);
+				} else {
+					$title = substr($fileData['filename'], 0, strlen($fileData['filename']) - 4); //Remove ".mp3"
+				}
+				
+				if (isset($fileData['id3v2']['comments']['artist'])) {
+					$artist = implode(", ", $fileData['id3v2']['comments']['artist']);
+				}
+				
+				if (isset($fileData['id3v2']['comments']['year'])) {
+					$year = implode(", ", $fileData['id3v2']['comments']['year']);
+				}
+				
+			//Converts something like 4578863 to 4.6
+				$size = sprintf("%.1f", intval($fileData['filesize']) / 1000000);
+				
+			//Cache the results
+				$wpdb->insert("ffi_aam_audiocache", array(
+					"Visible" => 1,
+					"Title" => $title,
+					"Artist" => $artist,
+					"Date" => $year,
+					"Length" => $fileData['playtime_string'],
+					"FileSize" => $size,
+					"FileName" => $fileData['filename']
+				));
+			}
+		}
+	}
+	
+/**
+ * This method will iterate through the $this->cache array and remove all
+ * cached entries which no longer exist on the file system.
+ * 
+ * @access private
+ * @return void
+ * @since  v1.0 Dev
+*/
+	
+	private function purgeOld() {
+		global $wpdb;
+		
+		if (count($this->cache)) {
+		//Build the query to delete old cached entries
+			$SQL = "DELETE FROM `ffi_aam_audiocache` WHERE";
+			$values = array();
+			
+			foreach($this->cache as $cache) {
+				$SQL .= " `FileName` = '%s' OR ";
+				array_push($values, $cache);
+			}
+			
+			$SQL = substr($SQL, 0, strlen($SQL) - 4);
+			
+		//Execute the query
+			$wpdb->query($wpdb->prepare($SQL, $values));
+		}
+	}
+	
+/**
+ * This method will find the differences between the files on the file
+ * system and cache. Files not in the file system will be stored in the
+ * $this->cache array, as a file listed in cache, but not in the file 
+ * system. Files not in cache will be stored in the $this->files array, 
+ * as the file located in the file system, but not listed in cache.
+ * 
+ * @access private
+ * @return void
+ * @since  v1.0 Dev
+*/
+	
+	private function diff() {
+	//Get all of the files in the file system and cache
 		$this->getAll();
 		$this->getCached();
-		$memCache = $this->cache;
 		
+	//Find any differences between the file system and cache
+		$memCache = $this->cache;
 		$this->cache = array_diff($this->cache, $this->files);
 		$this->files = array_diff($this->files, $memCache);
-		
-		print_r($this->cache);
-		print_r($this->files);
 	}
+	
+/**
+ * This method will put a sorted listing of all files in the file system
+ * in the $this->files array.
+ * 
+ * @access private
+ * @return void
+ * @since  v1.0 Dev
+*/
 	
 	private function getAll() {
 		$this->files = array();
@@ -42,10 +216,19 @@ class Audio_Caching_Manager {
 		sort($this->files);
 	}
 	
+/**
+ * This method will put a sorted listing of all files in cache in the 
+ * $this->cache array.
+ * 
+ * @access private
+ * @return void
+ * @since  v1.0 Dev
+*/
+	
 	private function getCached() {
 		global $wpdb;
 		
-		$this->cache = $wpdb->get_col($wpdb->prepare("SELECT File FROM ffi_aam_audiocache ORDER BY File ASC"));
+		$this->cache = $wpdb->get_col($wpdb->prepare("SELECT `FileName` FROM `ffi_aam_audiocache` ORDER BY `FileName` ASC"));
 	}
 }
 ?>
